3ad53e5a1a9188759423dda5f5777be320cf9b45
name-god <you@942887068@qq.com>
2019-08-14 11:46:51 +0800

    项目搭建完成

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tab-bar></tab-bar>
+  </div>
+</template>
+<script>
+import TabBar from "./components/Tab-bar";
+export default {
+  components: {
+    [TabBar.name]: TabBar
+  }
+};
+</script>
